99a59826c16619fd691fcbd2b97e21adc3d84696
kugi <kugimania@gmail.com>
2019-03-05 23:14:23 +0900

    06

---
+++ Roll-a-Ball/Assets/Script/GameContorller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameContorller : MonoBehaviour 
+{
+    public UnityEngine.UI.Text scoreLabel;
+	void Update () {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+        scoreLabel.text = count.ToString();
+	}
+}
