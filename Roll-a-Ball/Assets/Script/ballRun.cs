3674a2db7d9893db1cb87423b98b1372e77e82b9
kugi <kugimania@gmail.com>
2019-03-12 18:13:53 +0900

    ジャイロ

---
+++ Roll-a-Ball/Assets/Script/ballRun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballRun : MonoBehaviour
+{
+    private float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Vector3.zero;
+
+        dir.x = Input.acceleration.x;
+        dir.z = Input.acceleration.y;
+
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        dir *= Time.deltaTime;
+
+        transform.Translate(dir * speed);
+    }
+}
